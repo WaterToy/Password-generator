304dc99cc9c7f79a0a97aa3f5d55f9bb8ec88829
chenc <chenchuan94@outlook.com>
2018-11-06 17:40:41 +0800

    第一版

---
+++ README.md
@@ -0,0 +1,13 @@
+For personal use.
+
+target：password
+length：12
+contain：A~Za~z0~9.
+characteristic：The password is been calculated by the code, so it's decide by the character you input.
+
+Things you can control:
+    1. character 1, which usually to be the iconic abbreviation;
+    2. character 2, which is decided by you, actually it is recommended all your password should use the same character string.
+    3. you can say yes/no to the symbol '.'.
+
+Have fun!
--- main.py
+++ main.py
@@ -1,2 +1,44 @@
-import wxpy
-import jieba
+# 获取字母对应在字母表中的位置，从0开始计数
+def Cal_loc(a):
+    allStr = 'abcdefghijklmnopqrstuvwxyz'
+    return allStr.find(a)
+
+# 由 character 1 计算得到数字AB,A+B
+def Cal_AB(cha1):
+    lis = map(Cal_loc, cha1)
+    AB = sum(lis)%100
+    A_B = sum(lis)%10 + sum(lis)%100//10
+    return AB, A_B
+
+# 特征2位移A+B位
+def Shift_cha2(cha2, A_B):
+    Pri_Table = 'AzByCxDwEvFuGtHsIrJqKpLoMnNmOlPkQjRiShTgUfVeWdXcYbZa'
+    lis = list(map(Cal_loc, cha2))
+    for i,j in enumerate(lis):
+        lis[i] = Pri_Table[(j + A_B)%52]
+    return lis
+
+# 密码生成
+def Password_generate(cha1, cha2, cha3):
+    AB, A_B = Cal_AB(cha1)
+    letter = Shift_cha2(cha2, A_B)
+    letter.insert(AB%10, cha3)
+    c = ''
+    for i in letter:
+        c += str(letter)
+    password = c + str(AB)
+    return password
+
+print('''
+    密码由输入的特征计算得到，请务必记住自己输入的两组特征
+    建议 特征1 与目标站域名相关
+    建议 特征2 自己设置固定的字符串
+''')
+
+cha1 = input("请输入特征1：")
+cha2 = input("请输入特征2：")
+cha2 = 'chenchuan' if not len(cha2) else cha2
+cha3 = input("是否将‘.’加入密码中： yes/no\n--->")
+cha3 = '.' if not len(cha3) else ''
+password = Password_generate(cha1, cha2, cha3)
+print('密码：\n', password)
